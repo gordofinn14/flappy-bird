71f9993c66c6fbe80a52288f104fa65f5cdfed4c
gordofinn14 <gfinn14@seas.upenn.edu>
2019-04-21 19:42:23 -0400

    Reorganization of files and additional comments, need to include JUnit tests for design milestone

---
+++ src/application/BirdTest.java
@@ -0,0 +1,14 @@
+package application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BirdTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/application/Player.java
+++ src/application/Player.java
@@ -1,5 +1,52 @@
 package application;
 
-public class Player {
 
+/**
+ * Player Class
+ * 
+ * Attributes: High Score, Current Score, Player Name
+ * Methods: Getters, Setters, ImportScore, ExportScore
+ * @author 14gfi
+ *
+ */
+
+public class Player {
+	private int highScore;
+	private int currentScore;
+	private String playerName;
+	
+	public Player(int high, int current) {
+		highScore = high;
+		currentScore = current;
+	}
+
+	public int getHighScore() {
+		return highScore;
+	}
+
+	public void setHighScore(int highScore) {
+		this.highScore = highScore;
+	}
+
+	public int getCurrentScore() {
+		return currentScore;
+	}
+
+	public void setCurrentScore(int currentScore) {
+		this.currentScore = currentScore;
+	}
+	
+	/**
+	 * Import high score from text file saved down, if none, high score = 0 (try catch block)
+	 */
+	public void importHighScore() {
+		
+	}
+	
+	/**
+	 * If we have a new high score then we export to the scores text file 
+	 */
+	public void exportHighScore() {
+		
+	}
 }
